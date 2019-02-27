f29f6f23837ce2861648283f2cad3580adef04e7
Larissa <larissa@gasleuchte.de>
2019-02-27 21:11:13 +0100

    tried the bokeh tutorial and added figure to welcome

--- dataentry/views.py
+++ dataentry/views.py
@@ -4,9 +4,44 @@ from django.http import HttpResponse
 from django.db.models import Q
 #from django.template import loader
 from .models import Target, Country, PGAG
+from bokeh.plotting import figure, output_file, show
+from bokeh.embed import components
+from bokeh.models import ColumnDataSource, FactorRange, LassoSelectTool, WheelZoomTool, BoxSelectTool
+from bokeh.palettes import Spectral6
+from bokeh.transform import factor_cmap
 
 def welcome(request):
-    return render(request, 'dataentry/welcome.html')
+    fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+    years = ['2015', '2016', '2017']
+
+    data = {    'fruits'    : fruits,
+                '2015'      : [2, 1, 4, 3, 2, 4],
+                '2016'      : [5, 3, 3, 2, 4, 6],
+                '2017'      : [3, 2, 4, 4, 5, 3] 
+                }
+
+    x = [ (fruit, year) for fruit in fruits for year in years]
+    counts = sum(zip(data['2015'], data['2016'], data['2017']), ())
+
+    source = ColumnDataSource(data=dict(x=x, counts=counts))
+    plot = figure(x_range=FactorRange(*x), plot_height=250, title="Fruit Counts by Year",
+        toolbar_location="below", 
+        tools = "pan,wheel_zoom,box_zoom,reset")
+    plot.add_tools(BoxSelectTool(dimensions="width"))
+    #plot.add_tools(WheelZoomTool())
+    plot.vbar(x='x', top='counts', width=0.9, source=source, line_color="white",
+        fill_color=factor_cmap('x', palette=Spectral6, factors=years, start=1, end=2))
+
+    plot.y_range.start = 0
+    plot.x_range.range_padding = 0.1
+    plot.xaxis.major_label_orientation = 1
+    plot.xgrid.grid_line_color = None
+
+    script, div = components(plot)
+    return render(request, 'dataentry/welcome.html', {'script': script, 'div': div})
+
+#def welcome(request):
+#    return render(request, 'dataentry/welcome.html')
 
 def country(request):
     countries = Country.objects.all().order_by("name")
--- lokal/settings.py
+++ lokal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&v#9+)vg7cw9&pr_myrr#15#4!zge0mcs41^=n-$x5ni+a59d9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['militias-guidebook.com', '.pythonanywhere.com', '127.0.0.1']
 
